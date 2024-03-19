3ebf47a37d483e5c8bb3b55ac19dd43330c94e65
Artur Gafarov <gafarovartur@gmail.com>
2024-03-19 12:48:55 +0300

    feat: move Watcher to own file

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include "app/thread_pool.h"
 #include "app/crc32.h"
 #include "app/periodic_task.h"
+#include "app/watcher.h"
 
 namespace po = boost::program_options;
 namespace fs = std::filesystem;
@@ -86,98 +87,6 @@ inline std::string format(const std::string fmt, ...) {
 
 }
 
-class Watcher
-{
-public:
-    Watcher() {
-        m_fd = inotify_init();
-
-        if ( m_fd < 0 ) {
-            throw std::runtime_error("Cannot initialize inotify");
-        }
-    }
-    Watcher operator=(const Watcher&) = delete;
-
-    ~Watcher() {
-        for (auto& wd: m_wdDirMap) {
-            inotify_rm_watch( m_fd, wd.first );
-        }
-        close( m_fd );
-    }
-
-    void AddWatch(const std::string& path) {
-        int wd = inotify_add_watch(m_fd, path.c_str(), IN_DELETE | IN_CREATE | IN_MODIFY);
-        if ( wd < 0 ) {
-            throw std::runtime_error("Cannot add watch to the directory");
-        }
-        m_wdDirMap[wd] = path;
-    }
-
-    void SetCallback(const std::function< void(const std::string) > callback) {
-        m_fn = callback;
-    }
-
-    void RunWatcher () {
-        auto loop = [this]()
-        {
-            const size_t MAX_EVENTS = 1024;
-            const size_t EVENT_SIZE = sizeof(inotify_event) + NAME_MAX;
-            const size_t BUFF_SIZE = MAX_EVENTS * EVENT_SIZE;
-            auto buf = std::make_unique<char[]>(BUFF_SIZE);
-            char* buffer = buf.get();
-
-            auto handle_event = [this](const inotify_event* event)
-            {
-                const std::string path = format("%s/%s", m_wdDirMap[event->wd].c_str(), event->name);
-
-                if ( event->mask & IN_DELETE) {
-                    if (event->mask & IN_ISDIR)
-                        syslog(LOG_ERR, "Integrity check: FAIL (%s - the directory was removed)", path.c_str());
-                    else 
-                        syslog(LOG_ERR, "Integrity check: FAIL (%s - the file was removed)", path.c_str());
-                }
-                if ( event->mask & IN_CREATE) {
-                    if (event->mask & IN_ISDIR)
-                        syslog(LOG_INFO, "The directory %s was created", path.c_str());
-                    else {
-                        syslog(LOG_INFO, "The file %s was created, recalculating", path.c_str());
-                        m_fn(path);
-                    }
-                }
-                if ( event->mask & IN_MODIFY) {
-                    if (event->mask & IN_ISDIR)
-                        syslog(LOG_INFO, "The directory %s was modified", path.c_str());
-                    else {
-                        syslog(LOG_INFO, "The file %s was modified, recalculating", path.c_str());
-                        m_fn(path);
-                    }
-                }
-            };
-
-            while (true) {
-                int size = read(m_fd, buffer, BUFF_SIZE);
-
-                int i = 0;
-                while(i < size) {
-                    inotify_event* event = (inotify_event*)&buffer[i];
-                    if ( event->len ) {
-                        handle_event(event);
-                        i += sizeof(inotify_event) + event->len;
-                    }
-
-                }
-            }
-        };
-        std::thread watchThread(loop);
-        watchThread.detach();
-    }
-
-private:
-
-    std::function< void(const std::string) > m_fn;
-    int m_fd;
-    std::unordered_map<int, std::string> m_wdDirMap;
-};
 
 class Application : public Watcher {
 
